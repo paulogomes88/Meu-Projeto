ef8f2296b577f3ae4f5cc193f61ceea9015e1f95
Paulo Roberto Gomes <93014840+paulogomes88@users.noreply.github.com>
2025-11-19 14:33:51 +0000

    19/11/2025 11:33

--- N3/n3_hora_de_codar.c
+++ N3/n3_hora_de_codar.c
@@ -17,23 +17,35 @@ int main(){
     float valorA = 10.50;
     float valorB = 20.40;
 
-    unsigned int estoqueMininoA = 500;
+    unsigned int estoqueMinimoA = 500;
     unsigned int estoqueMinimoB = 2500;
 
-    double valorTotalA;
-    double valorTotalB;
+    double valorTotalA = estoqueA * valorA;
+    double valorTotalB = estoqueB * valorB;
 
     int resultadoA, resultadoB;
 
     // Exibir as informações na tela
+    printf("*** Exibição ***\n");
     printf("Produto %s tem estoque %u e o valor unitário é %.2f\n", produtoA, estoqueA, valorA);
     printf("Produto %s tem estoque %u e o valor unitário é %.2f\n", produtoB, estoqueB, valorB);
 
     // Comparação com valor mínimo de estoque
+    resultadoA = estoqueA > estoqueMinimoA;
+    resultadoB = estoqueB > estoqueMinimoB;
+
+    printf("*** Comparação com valor mínimo de estoque ***\n");
+    printf("O produto %s tem estoque mínimo %d\n", produtoA, estoqueMinimoA);
+    printf("O produto %s tem estoque mínimo %d\n", produtoB, estoqueMinimoB);
 
 
     // Comparação entre os valores totais dos produtos
+    printf("*** Comparação entre os valores totais dos produtos ***\n");
 
+    printf("O valor total de A (R$ %.2f) é maior que o valor total de B (R$ %.2f)? %d",
+        valorTotalA, valorTotalB, (valorTotalA > valorTotalB)
+    );
     
-    
+    printf("\n*** Fim ***\n");
+    return 0;
 }
