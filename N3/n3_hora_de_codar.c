b7f373de5af88a36a20ea747d7b01bb69a9aa588
Paulo Roberto Gomes <93014840+paulogomes88@users.noreply.github.com>
2025-11-19 13:55:53 +0000

    19/11/2025 10:55 Atualização

---
+++ N3/n3_hora_de_codar.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+Programa de Inventário para comparação de produtos em estoque
+
+cadastrar produtos 
+comparar estoques
+operadores relacionais para comparação
+utilização do undisgned
+*/
+
+int main(){
+    char produtoA[30] = "Produto A";
+    char produtoB[30] = "Produto B";
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
+    float valorA = 10.50;
+    float valorB = 20.40;
+
+    unsigned int estoqueMininoA = 500;
+    unsigned int estoqueMinimoB = 2500;
+
+    double valorTotalA;
+    double valorTotalB;
+
+    // Exibir as informações na tela
+    printf("Produto %s tem estoque %u e o valor unitário é %.2f\n", produtoA, estoqueA, valorA);
+    printf("Produto %s tem estoque %u e o valor unitário é %.2f\n", produtoB, estoqueB, valorB);
+
+    // Comparação com valor mínimo de estoque
+
+
+    // Comparação entre os valores totais dos produtos
+
+    
+    
+}
